f64ca00904ab7fc719bd2e083d51a4f78d173b94
Akashmahto022 <akashmahto1169@gmail.com>
2024-12-15 13:14:08 +0530

    create otp verify model

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 import Link from 'next/link';
 import { createAccount } from '@/lib/actions/user.actions';
+import OTPModal from './OTPModal';
 
 type FormType = 'sign-up' | 'sign-in';
 
@@ -33,7 +34,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-  const [accountId, setAccountId] = useState(null)
+  const [accountId, setAccountId] = useState(null);
 
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
@@ -47,17 +48,17 @@ const AuthForm = ({ type }: { type: FormType }) => {
   // Define a submit handler.
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    setErrorMessage("")
+    setErrorMessage('');
     try {
       const user = await createAccount({
-        fullName: values.fullName || "",
+        fullName: values.fullName || '',
         email: values.email,
-      })
-      setAccountId(user.accountId)
+      });
+      setAccountId(user.accountId);
     } catch (error) {
-      setErrorMessage("Faild to create account. Please tru again.")
-    }finally{
-      setIsLoading(false)
+      setErrorMessage('Faild to create account. Please tru again.');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -144,6 +145,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
       </Form>
 
       {/* otp verification */}
+
+      {true && (
+        <OTPModal email={form.getValues('email')} accountId={accountId} />
+      )}
     </>
   );
 };
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,63 +1,68 @@
-"use server";
+'use server';
 
-import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
-import { appwriteConfig } from "../appwrite/config";
-import { parseStringify } from "../utils";
+import { ID, Query } from 'node-appwrite';
+import { createAdminClient } from '../appwrite';
+import { appwriteConfig } from '../appwrite/config';
+import { parseStringify } from '../utils';
 
 const getUserByEmail = async (email: string) => {
-    const { databases } = await createAdminClient();
+  const { databases } = await createAdminClient();
 
-    const result = await databases.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.userCollectionId,
-        [Query.equal("email", ["email"])]
-    )
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    [Query.equal('email', ['email'])]
+  );
 
-    return result.total > 0 ? result.documents[0] : null;
-}
+  return result.total > 0 ? result.documents[0] : null;
+};
 
 const habdleError = (error: unknown, message: string) => {
-    console.log(error, message)
+  console.log(error, message);
 
-    throw error;
-}
+  throw error;
+};
 
 const sendEmailOtp = async ({ email }: { email: string }) => {
-    const { account } = await createAdminClient();
+  const { account } = await createAdminClient();
 
-    try {
-        const session = await account.createEmailToken(ID.unique(), email)
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
 
-        return session.userId
-    } catch (error) {
-        habdleError(error, "failed to send email OTP");
-    }
-}
+    return session.userId;
+  } catch (error) {
+    habdleError(error, 'failed to send email OTP');
+  }
+};
 
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
 
-export const createAccount = async ({ fullName, email }: { fullName: string; email: string; }) => {
-    const existingUser = await getUserByEmail(email)
+  const accountId = await sendEmailOtp({ email });
 
-    const accountId = await sendEmailOtp({ email })
+  if (!accountId) throw new Error('Failed to send an otp');
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
 
-    if (!accountId) throw new Error("Failed to send an otp");
-    if (!existingUser) {
-        const { databases } = await createAdminClient();
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          'https://api-private.atlassian.com/users/9cea692d0a59c5e100680165cbbeb496/avatar',
+        accountId,
+      }
+    );
+  }
 
-        await databases.createDocument(
-            appwriteConfig.databaseId,
-            appwriteConfig.userCollectionId,
-            ID.unique(),
-            {
-                fullName,
-                email,
-                avatar: "https://api-private.atlassian.com/users/9cea692d0a59c5e100680165cbbeb496/avatar",
-                accountId,
-            }
-        )
-    }
-
-    return parseStringify({ accountId })
-
-}
+  return parseStringify({ accountId });
+};
