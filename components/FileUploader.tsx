74daa72b714735abadad5b0157bd7b51be347fe8
Akashmahto022 <akashmahto1169@gmail.com>
2025-02-05 10:54:36 +0530

    created a file uploading functionality

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -3,23 +3,66 @@
 import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { Button } from './ui/button';
-import { cn, getFileType } from '@/lib/utils';
+import { cn, convertFileToUrl, getFileType } from '@/lib/utils';
 import Image from 'next/image';
+import Thumbnail from './Thumbnail';
+import { MAX_FILE_SIZE } from '@/constants';
+import { useToast } from "@/hooks/use-toast"
+import { uploadFile } from '@/lib/actions/file.action';
+import { usePathname } from 'next/navigation';
+
 
 interface Props {
   ownerId: string,
   accountId: string,
-  className: string
+  className?: string
 }
 
 const FileUploader = ({ ownerId, accountId, className }: Props) => {
 
   const [files, setFiles] = useState<File[]>([])
+  const { toast } = useToast()
+  const path = usePathname()
 
-  const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-  }, [])
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+  const onDrop = useCallback(async (acceptedFiles: File[]) => {
+    setFiles(acceptedFiles);
+
+    const uploadPromises = acceptedFiles.map(async (file) => {
+      if (file.size > MAX_FILE_SIZE) {
+        setFiles((prevFiles) => prevFiles.filter((f) => {
+          f.name != file.name
+        }))
+
+        return toast({
+          description: (
+            <p className='body-2 text-white'>
+              <span className='font-semibold'>
+                {file.name}
+
+              </span> is too large. Max file size is 50MB.
+            </p>
+          ), className: "error-toast",
+        })
+      }
+
+      return uploadFile({ file, ownerId, accountId, path }).then((uploadFile) => {
+        if (uploadFile) {
+          setFiles((prevfile) =>
+            prevfile.filter((f) => f.name != file.name)
+          );
+        }
+      })
+    })
+    await Promise.all(uploadPromises)
+  }, [ownerId, accountId, path])
+
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
+
+  const handleRemoveFile = (e: React.MouseEvent<HTMLImageElement, MouseEvent>, fileName: string) => {
+    e.stopPropagation();
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name != fileName))
+  }
 
   return (
     <div {...getRootProps()} className='cursor-pointer'>
@@ -36,18 +79,29 @@ const FileUploader = ({ ownerId, accountId, className }: Props) => {
               files.map((file, index) => {
                 const { type, extension } = getFileType(file.name);
                 return (
-                  <li key={`${file.name}-${index}`}></li>
+                  <li key={`${file.name}-${index}`} className='uploader-preview-item' >
+                    <div className='flex items-center gap-3'>
+                      <Thumbnail
+                        type={type}
+                        extension={extension}
+                        url={convertFileToUrl(file)}
+                      />
+                      <div className='preview-item-name'>
+                        {file.name}
+                        <Image src={"/assets/icons/file-loader.gif"} alt='loader' width={80} height={26} />
+                      </div>
+                    </div>
+
+                    <Image src={"/assets/icons/remove.svg"} alt='remove' width={24} height={24} onClick={(e) => handleRemoveFile(e, file.name)} />
+
+                  </li>
                 )
               })
             }
           </ul>
         )
       }
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
+
     </div>
   )
 };
--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -66,7 +66,7 @@ const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email }: Props
             </nav>
             <Separator className='my-5 bg-light-200/20' />
             <div className='flex flex-col justify-between gap-5 pb-5'>
-              <FileUploader />
+              <FileUploader ownerId={ownerId} accountId={accountId} />
               <Button type='submit' className='mobile-sign-out-button' onClick={async () => await signOutUser()}>
                 <Image src={"/assets/icons/logout.svg"} alt='logo' width={24} height={24} />
                 <p>Logout</p>
